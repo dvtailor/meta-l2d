16fc8cd5598be9ae406cfa6e6d94bd0ae2eec99c
dvtailor <dharmesh.tailor@live.co.uk>
2023-10-23 00:26:07 +0200

    minor

--- main_gtsrb.py
+++ main_gtsrb.py
@@ -540,8 +540,8 @@ if __name__ == "__main__":
     parser.add_argument("--warmstart_epochs", type=int, default=100)
 
     ## EVAL
-    # parser.add_argument('--budget', nargs='+', type=float, default=[0.01,0.05,0.1,0.2,1.0])
-    parser.add_argument('--budget', nargs='+', type=float, default=[1.0])
+    parser.add_argument('--budget', nargs='+', type=float, default=[0.01,0.02,0.05,0.1,0.2,0.5]) # 1.0
+    # parser.add_argument('--budget', nargs='+', type=float, default=[1.0])
 
     parser.add_argument('--finetune_single', action='store_true')
     parser.set_defaults(finetune_single=True)
