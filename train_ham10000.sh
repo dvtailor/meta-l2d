a4586d2cb6ba0a6a793f458ef0935cbe51658849
dvtailor <dharmesh.tailor@live.co.uk>
2024-01-30 16:57:32 +0100

    ham10000 bash

---
+++ train_ham10000.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+l2d=$1
+p_out=$2
+mode=$3
+seed=$4
+
+train_batch_size=128
+lr_wrn=1e-2
+lr_other=1e-3
+dataset=ham10000
+val_batch_size=8
+test_batch_size=1
+warmstart=false
+n_cntx_pts=140
+epochs=200
+
+
+flags=''
+if [ "${warmstart}" = true ]; then
+    flags="${flags} --warmstart"
+fi
+
+python main.py --l2d=${l2d} --p_out=${p_out} --mode=${mode} --seed=${seed} \
+               --train_batch_size=${train_batch_size} --lr_wrn=${lr_wrn} --lr_other=${lr_other} \
+               --dataset=${dataset} --val_batch_size=${val_batch_size} --test_batch_size=${test_batch_size} \
+               --n_cntx_pts=${n_cntx_pts} --epochs=${epochs} ${flags}
